ad2d99b71b611534a30d692a521eb70e58212218
Lisa Wong <miss.lisa19@hotmail.com>
2016-03-13 09:12:58 -0700

    support page

--- iHeartClient/app/components/Support.js
+++ iHeartClient/app/components/Support.js
@@ -4,6 +4,7 @@ import React, {
 	Text,
 	TextInput,
 	TouchableOpacity,
+	StyleSheet,
 } from 'react-native'
 import style from './../style/style'
 import Meteor, {connectMeteor, } from 'react-native-meteor'
@@ -18,7 +19,7 @@ export default class Support extends Component {
 		}
 	}
 
-	submit(){
+	signOut(){
 	}
 
 	componentWillMount(){
@@ -27,12 +28,53 @@ export default class Support extends Component {
 
 	render(){
 		return(
-			<View style={style.center}>
-			
- 				<TouchableOpacity onPress={this.submit}>
- 				<Text>Submit</Text>
+			<View style={styles.container}>
+				<Text style={styles.text}>Support</Text>
+
+				<View style={styles.info}>
+				<Text style={styles.text}>Cardiologist Contact:</Text>
+				<Text style={styles.text}>Dr. Annie Chou</Text>
+				<Text style={styles.text}>Phone number: 778-923-0909</Text>
+				<Text style={styles.text}>Email: annie@cardiology.com</Text>
+				<Text style={styles.text}>Resources:</Text>
+				<Text style={style.text}>http://www.ubccardio.com/</Text>
+				</View>
+
+				<View style={styles.button}>
+ 				<TouchableOpacity onPress={this.signOut.bind(this)} style={styles.box}>
+ 				<Text style={styles.text}>Sign Out</Text>
  				</TouchableOpacity>
+ 				</View>
 			</View>
 		)
 	}
 }
+
+let styles = StyleSheet.create({
+	container:{
+		flex:1, 
+		alignItems:'center',
+		margin: 75
+			},
+
+	button:{
+		borderWidth:2,
+		borderColor:'black',
+		margin: 20
+	}, 
+
+	text:{
+		color:'black',
+	},
+
+	box:{
+		height:45,
+		width:200,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	info:{
+
+	}
+})
