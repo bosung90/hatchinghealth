848dee3edb7bf20184e4fa36170f7c0fcfa02e68
Eric Kim <bosung90@gmail.com>
2016-03-13 03:16:18 -0700

    default data for patient and patient records

--- iHeartServer/server/app.jsx
+++ iHeartServer/server/app.jsx
@@ -1,10 +1,30 @@
 Meteor.startup(()=>{
-	let userId = Accounts.findUserByEmail('annie@example.com')
-	if(!userId) {
-		userId = Accounts.createUser({email: 'annie@example.com', password: '123'})
-		Doctors.insert({userId, phoneNumber: '778-111-2222', name: 'Annie Chou'})
+	let doctorUserId = Accounts.findUserByEmail('annie@example.com')
+	if(!doctorUserId) {
+		doctorUserId = Accounts.createUser({email: 'annie@example.com', password: '123'})
+		Doctors.insert({userId: doctorUserId, phoneNumber: '778-111-2222', name: 'Annie Chou'})
 	}
-	
 
+	let patientUserId = Accounts.findUserByEmail('lisa@example.com')
+	if(!patientUserId) {
+		patientUserId = Accounts.createUser({email: 'lisa@example.com', password: '123'})
+		Patients.insert({userId: patientUserId, doctorId: doctorUserId, phoneNumber: '778-333-4444', name: 'Lisa Wong', baseWeight: 60})
+
+		let date = new Date();
+		date.setDate(date.getDate() - 1);
+		PatientRecords.insert({patientId: patientUserId, weight: '68', flag: true, measuredDate: date})
+
+		date.setDate(date.getDate() - 1);
+		PatientRecords.insert({patientId: patientUserId, weight: '66', flag: false, measuredDate: date})
+
+		date.setDate(date.getDate() - 1);
+		PatientRecords.insert({patientId: patientUserId, weight: '64', flag: false, measuredDate: date})
+
+		date.setDate(date.getDate() - 1);
+		PatientRecords.insert({patientId: patientUserId, weight: '62', flag: false, measuredDate: date})
+
+		date.setDate(date.getDate() - 1);
+		PatientRecords.insert({patientId: patientUserId, weight: '60', flag: false, measuredDate: date})
+	}
 	
 })
